037a2aa941345f3829eae80fdb6c7c46fdd44fef
David Moser <david@theworkingcomputer.com>
2015-06-12 10:03:24 -0400

    added custom domain data

--- pelicanconf.py
+++ pelicanconf.py
@@ -12,6 +12,16 @@ TIMEZONE = 'Europe/Paris'
 
 DEFAULT_LANG = u'en'
 
+# Static paths will be copied without parsing
+
+STATIC_PATHS = ['images', 'extra']
+
+# Shift the installed location of a file
+
+EXTRA_PATH_METADATA = {
+    'extra/CNAME': {'path': 'CNAME'},
+}
+
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
 CATEGORY_FEED_ATOM = None
