6be324cb67ea9f3c58dc31846b2e343c4394271c
David Moser <david@theworkingcomputer.com>
2015-06-13 12:46:23 -0400

    fine tuning initial workflow

--- pelicanconf.py
+++ pelicanconf.py
@@ -44,7 +44,7 @@ SOCIAL = (('You can add links in your config file', '#'),
 DEFAULT_PAGINATION = 4
 
 # Uncomment following line if you want document-relative URLs when developing
-#RELATIVE_URLS = True
+RELATIVE_URLS = True
 
 # Contact
 EMAIL_ADDR = 'david at theworkingcomputer dot com'
