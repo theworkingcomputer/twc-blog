666146f0b7014a6fd2e88de0c766092e03b4934f
David Moser <david@theworkingcomputer.com>
2015-06-25 14:44:21 -0400

    pelicanconf.py adjustment

--- publishconf.py
+++ publishconf.py
@@ -20,5 +20,5 @@ DELETE_OUTPUT_DIRECTORY = True
 
 # Following items are often useful when publishing
 
-DISQUS_SITENAME = "twc-blog.disqus.com"
+DISQUS_SITENAME = "twc-blog.theworkingcouputer.disqus.com"
 #GOOGLE_ANALYTICS = ""
